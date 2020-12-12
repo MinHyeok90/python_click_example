5dbb6e4f9ee303e0bccf38166b3f65de32045024
minhyukKim <dufflaa@gmail.com>
2020-12-13 02:56:06 +0900

    Use Group

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,27 @@
 import click
 
-@click.command()
+@click.group()
 def cli():
     """
         Hello cli space!
     """
-    click.echo("hello world")
+
+
+@cli.command("command1")
+def command1():
+    """
+        Hello cli space!
+    """
+    click.echo("Command1")
+
+
+@cli.command("command2")
+def command2():
+    """
+        Hello cli space!
+    """
+    click.echo("Command2")
+
 
 if __name__ == '__main__':
     cli()
