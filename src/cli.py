5a13d705514fae79936cc45f1328bd3382f349bd
minhyukKim <dufflaa@gmail.com>
2020-12-13 03:55:07 +0900

    Add option autocomplete(Choice)

--- src/cli.py
+++ src/cli.py
@@ -17,7 +17,7 @@ def cli(ctx):
 
 @cli.command("command1")
 @click.argument("arg1", autocompletion=get_path)
-@click.option("-o", "--option", default="hello")
+@click.option("-o", "--option", type=click.Choice(["hello", "world"], case_sensitive=False), default="hello")
 def command1(arg1, option):
     """
         Hello cli space!
